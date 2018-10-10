02cabc1fe35f6818d9c0a137a0e7d7bcae7916e7
lata <panwarlsweet@gmail.com>
2018-10-10 06:03:33 +0200

    change in code to run on Data

--- Systematics/python/SystematicDumperDefaultVariables.py
+++ Systematics/python/SystematicDumperDefaultVariables.py
@@ -1,5 +1,5 @@
 minimalVariables = ["CMS_hgg_mass[160,100,180]:=diPhoton().mass",
-                    "dZ[40,-20.,20.]:=(tagTruth().genPV().z-diPhoton().vtx().z)", # store actual value
+                    #"dZ[40,-20.,20.]:=(tagTruth().genPV().z-diPhoton().vtx().z)", # store actual value
                                                                                #when doing systematics, variables need to have a binning
                                                                                #specified, otherwise the rooDataHist end up empty.
             								       #an assert in the code prevents you from doing this.
@@ -16,9 +16,9 @@ defaultVariables=["CMS_hgg_mass[160,100,180]:=diPhoton().mass",
                                     "subleadPt                :=diPhoton().subLeadingPhoton.pt",
                                     "diphoMVA                 :=diPhotonMVA().result",    
                                     "maxEta                   :=max(abs(diPhoton().leadingPhoton.superCluster.eta),abs(diPhoton().leadingPhoton.superCluster.eta))",
-                                    "genZ           :=tagTruth().genPV().z",
+                                    #"genZ           :=tagTruth().genPV().z",
                                     "vtxZ           :=diPhoton().vtx().z",
-                                    "dZ             :=(tagTruth().genPV().z-diPhoton().vtx().z)"]
+                                    #"dZ             :=(tagTruth().genPV().z-diPhoton().vtx().z)"]
 
 
 defaultHistograms=["CMS_hgg_mass>>mass(160,100,180)",
