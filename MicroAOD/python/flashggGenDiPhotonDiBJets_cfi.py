fd0d9879464b013983215ea8935a70c2d9931589
lata <panwarlsweet@gmail.com>
2019-05-15 16:31:39 +0200

    calling PAT genejets to fill gen info

--- MicroAOD/python/flashggGenDiPhotonDiBJets_cfi.py
+++ MicroAOD/python/flashggGenDiPhotonDiBJets_cfi.py
@@ -2,7 +2,7 @@ import FWCore.ParameterSet.Config as cms
 
 flashggGenDiPhotonDiBJets = cms.EDProducer('FlashggGenDiPhotonDiJetProducer',
                                      src=cms.InputTag('flashggGenPhotonsExtra'),
-                                     jets=cms.InputTag('flashggHiggsBJets'),
+                                     jets=cms.InputTag('slimmedGenJets'),
                                      flashggMets=cms.InputTag('flashggMets'),
                                      flashggHiggsBJets=cms.InputTag('flashggHiggsBJets'),
                                      )
