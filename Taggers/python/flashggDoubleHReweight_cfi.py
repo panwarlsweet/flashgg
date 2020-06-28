62fbf53a81420a7cca0f0e764788830e943a4107
Lata <panwarlsweet@gmail.com>
2020-06-28 07:11:20 +0200

    adding KL scan

--- Taggers/python/flashggDoubleHReweight_cfi.py
+++ Taggers/python/flashggDoubleHReweight_cfi.py
@@ -19,4 +19,4 @@ flashggDoubleHReweight = cms.EDProducer("FlashggDoubleHReweighter",
                                         )
 
 reweight_producer = "flashggDoubleHReweight"
-reweight_names = ["benchmark0","benchmark1","benchmark2","benchmark3","benchmark4","benchmark5","benchmark6","benchmark7","benchmark8","benchmark9","benchmark10","benchmark11","benchmarkSM","benchmarkBox","benchmark2017fake"]
+reweight_names = ["benchmark0","benchmark1","benchmark2","benchmark3","benchmark4","benchmark5","benchmark6","benchmark7","benchmark8","benchmark9","benchmark10","benchmark11","benchmarkSM","benchmarkBox","benchmark2017fake","KL1","KL2","KL3","KL4","KL5","KL6","KL7","KL8","KL9","KL10","KL11","KL12","KL13","KL14","KL15","KL16","KL17","KL18","KL19","KL20","KL21","KL22","KL23","KL24","KL25","KL26","KL27","KL28","KL29","KL30","KL31","KL32","KL33","KL34","KL35","KL36","KL37","KL38","KL39","KL40","KL41","KL42","KL43","KL44","KL45","KL46","KL47","KL48","KL49","KL50","KL51","KL52","KL53","KL54","KL55","KL56","KL57","KL58","KL59","KL60","KL61","KL62","KL63","KL64","KL65","KL66","KL67","KL68","KL69","KL70","KL71","KL72","KL73","KL74","KL75","KL76","KL77","KL78","KL79","KL80"]
